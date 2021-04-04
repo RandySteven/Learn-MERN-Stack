b7357199a4e2f024a8af7ea3b6d4909badf74c7b
Randy Steven <randysteven12@gmail.com>
2021-04-04 15:59:47 +0700

    fetch API GET

--- mern-blog/src/components/molecules/BlogItem/index.js
+++ mern-blog/src/components/molecules/BlogItem/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { useHistory } from 'react-router'
 import { RegisterBg } from '../../../assets'
 import './BlogItem.scss'
-const BlogItem = () => {
+const BlogItem = (props) => {
+    const history = useHistory();
+    const {image, title, name, date, body} = props;
     return (
         <div className="blog-item">
-            <img className="image-thumb" src={RegisterBg} alt="post" />
+            <img className="image-thumb" src={image} alt="post" />
             <div className="content-detail">
-                <p className="title">Title Blog</p>
-                <p className="author">Author - Date Post</p>
-                <p className="body">lorem ipsum</p>
+                <p className="title">{title}</p>
+                <p className="author">{name} - {date} </p>
+                <p className="body"> {body} </p>
             </div>
         </div>
     )
--- mern-blog/src/pages/home/index.js
+++ mern-blog/src/pages/home/index.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import Axios from 'axios'
+import React, {useEffect, useState} from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useHistory } from 'react-router'
 import { BlogItem, Button, Gap } from '../../components'
 import './home.scss'
+
 const Home = () => {
+    const {dataBlogs, name} = useSelector(state => state);
+    const dispatch = useDispatch()
+    console.log('data blog global: ', dataBlogs)
+    useEffect(()=> {
+        setTimeout(() => {
+            dispatch({type:'UPDATE_NAME'})
+        }, 3000)
+
+        Axios.get('http://localhost:4000/v1/blog/posts?page=2')
+        .then(result => {
+            console.log('data API ', result.data);
+            const responseAPI = result.data;
+
+            dispatch({type:'UPDATE_DATA_BLOG', payload: responseAPI.data})
+        })
+        .catch(err => {
+            console.log('Error: ', err);
+        })
+    }, [dispatch])
+    const history = useHistory();
     return (
         <div className="home-wrapper">
             <div className="create-wrapper">
                 <Button title="create blog"/>
             </div>
+            <p>{name}</p>
             <Gap height={20} />
             <div className="content-wrapper">
-                <BlogItem />
-                <BlogItem />
-                <BlogItem />
-                <BlogItem />
-                <BlogItem />
-                <BlogItem />
+                {dataBlogs.map(blog => {
+                    return <BlogItem key={blog._id} 
+                    image={`http://localhost:4000/${blog.image}`} 
+                    title={blog.title} 
+                    body={blog.body}
+                    name={blog.author.name}
+                    date={blog.createdAt}
+                    />
+                })}
             </div>
             <div className="pagination">
                 <Button title="Previous" />
