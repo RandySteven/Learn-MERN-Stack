e015e0a021f6957ce392116695fe6c20eb3dafe7
Randy Steven <randysteven12@gmail.com>
2021-04-04 15:00:39 +0700

    pagination

--- mern-api/src/controllers/blog.js
+++ mern-api/src/controllers/blog.js
@@ -49,16 +49,30 @@ exports.createBlogPost = (req, res, next) => {
 }
 
 exports.getAllBlogPost = (req, res, next) => {
-    BlogPost.find()
+    const currentPage = req.query.page || 1;
+    const perPage = req.query.perPage || 5
+    let totalItems;
+
+    BlogPost.find().countDocuments()
+        .then(count => {
+            totalItems = count;
+            return BlogPost.find()
+            .skip((parseInt(currentPage) - 1) * parseInt(perPage))
+            .limit(parseInt(perPage));
+        })
         .then(result => {
             res.status(200).json({
                 message: 'Data Blog Post Berhasil dipanggil',
-                data: result
+                total_data: totalItems,
+                per_page: parseInt(perPage),
+                current_page: parseInt(currentPage),
+                data: result,
             })
         })
         .catch(err => {
-            next(err);
+            next(err)
         })
+
 }
 
 exports.getBlogPostById = (req, res, next) => {
